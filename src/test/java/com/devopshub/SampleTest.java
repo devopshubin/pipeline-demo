61e2779d386b2f049650793c4c4d39512e82de01
Sreyas3599 <sreyasp3599@gmail.com>
2026-02-12 01:56:58 +0530

    Added sample Junit test

---
+++ src/test/java/com/devopshub/SampleTest.java
@@ -0,0 +1,12 @@
+package com.devopshub;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void testAlwaysPass() {
+        assertTrue(true);
+    }
+}
